19c9f5c9ec727e25006386cafc79c166d227d2dd
unknown <luisd824@gmail.com>
2018-05-11 13:27:46 -0400

    got heap test to pass. some answers on md

--- algorithms/Algorithms_Answers.md
+++ algorithms/Algorithms_Answers.md
@@ -1,1 +1,21 @@
 Add your answers to the Algorithms exercises here.
+
+Exc.1 
+
+    a.
+
+    b. O(log n)
+
+    c. O(n^2)
+
+    d. O(n^2)
+
+    e. O(2^n)
+
+    f. O(1)
+
+    g. O(n)
+
+Exc.2 
+
+a. 
--- data_structures/src/heap.js
+++ data_structures/src/heap.js
@@ -1,18 +1,63 @@
-const heapsort = (arr) => {
+const heapsort = arr => {
   /* Your code here */
-  
-};
 
-class Heap {
-  constructor() {
-    this.storage = [null];
-    this.size = 0;
+    let n = arr.length;
+  // loop to rearrange array  
+    for (let i = n / 2 -1; i >= 0; i--) {
+      heapify(arr, n, i);
+    }
+    
+    // start from end, move to root
+    for (let i = n - 1; i >= 0; i--) {
+    
+    // move root to end  
+    let temp = arr[0];
+    arr[0] = arr[i];
+    arr[i] = temp;
+
+    // call heapify to make sure max is moved    
+    heapify(arr, i, 0);
+    }
+    return arr;
+};
+  
+  
+  function heapify(arr, n, i) {
+    // max being root
+    let max = i;
+    let l = 2 * i + 1;
+    let r = 2 * i + 2;
+    
+    if(l < n && arr[i] < arr[l]) {
+      max = l;
+    }
+    
+    if(r < n && arr[max] < arr[r]) {
+      max = r;
+    }
+
+    // check if max number is root
+    if(max !== i) {
+      let swap = arr[i];
+      arr[i] = arr[max];
+      arr[max] = swap;
+      // recursive call on current element or child
+      heapify(arr, n , max);
+    }
   }
+    
+  
+  class Heap {
+    constructor() {
+      this.storage = [null];
+      this.size = 0;
+    }
 
   insert(val) {
     const index = this.storage.push(val) - 1;
     this.size++;
     this.bubbleUp(index);
+
   }
 
   delete() {
@@ -31,6 +76,7 @@ class Heap {
 
   getMax() {
     return this.storage[1];
+
   }
 
   getSize() {
@@ -38,9 +84,12 @@ class Heap {
   }
 
   bubbleUp(index) {
-    const parent = Math.floor(index/2);
+    const parent = Math.floor(index / 2);
     if (parent > 0 && this.storage[parent] < this.storage[index]) {
-      [this.storage[parent], this.storage[index]] = [this.storage[index], this.storage[parent]];
+      [this.storage[parent], this.storage[index]] = [
+        this.storage[index],
+        this.storage[parent]
+      ];
       this.bubbleUp(parent);
     }
   }
@@ -54,11 +103,15 @@ class Heap {
       if (this.storage[child2] === undefined) {
         maxChild = child1;
       } else if (this.storage[child2] !== undefined) {
-        maxChild = this.storage[child1] > this.storage[child2] ? child1 : child2;
+        maxChild =
+          this.storage[child1] > this.storage[child2] ? child1 : child2;
       }
 
       if (this.storage[index] < this.storage[maxChild]) {
-        [this.storage[maxChild], this.storage[index]] = [this.storage[index], this.storage[maxChild]];
+        [this.storage[maxChild], this.storage[index]] = [
+          this.storage[index],
+          this.storage[maxChild]
+        ];
         this.siftDown(maxChild);
       }
     }
@@ -67,5 +120,5 @@ class Heap {
 
 module.exports = {
   Heap,
-  heapsort,
+  heapsort
 };
